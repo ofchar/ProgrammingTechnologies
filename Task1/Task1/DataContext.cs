9c3e47260c5751af374c0c0be4dadb0003944df8
ofchar <matowczarek@outlook.com>
2021-03-15 11:20:34 +0100

    Basic dataContext

--- Task1/Task1/DataContext.cs
+++ Task1/Task1/DataContext.cs
@@ -8,5 +8,9 @@ namespace Task1
 {
     class DataContext
     {
+        public List<User> users = new List<User>();
+        public List<Catalog> catalogs = new List<Catalog>();
+        public List<Event> events = new List<Event>();
+        public List<State> states = new List<State>();
     }
 }

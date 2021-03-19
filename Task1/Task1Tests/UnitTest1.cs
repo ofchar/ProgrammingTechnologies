f6a0729bbcedfae56703f99719dd6ef638ff3128
ofchar <matowczarek@outlook.com>
2021-03-19 22:11:23 +0100

    Created unit test project for Task1

---
+++ Task1/Task1Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task1Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DataRepositoryTests()
+        {
+
+        }
+    }
+}

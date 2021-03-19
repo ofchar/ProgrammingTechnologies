0613332ae03ff6f525740d7bd94d3d6412d6cef7
ofchar <matowczarek@outlook.com>
2021-03-19 12:42:50 +0100

    DataService constructor and Display, Add and Create methods

--- Task1/Task1/DataService.cs
+++ Task1/Task1/DataService.cs
@@ -8,5 +8,64 @@ namespace Task1
 {
     class DataService
     {
+        private DataRepository repository;
+
+        public DataService(DataRepository repository)
+        {
+            this.repository = repository;
+        }
+
+
+        #region Display
+        public void Display(IEnumerable<User> users)
+        {
+            foreach(User user in users)
+            {
+                Console.WriteLine(user.All);
+            }
+        }
+        public void Display(IEnumerable<Catalog> catalogs)
+        {
+            foreach (Catalog catalog in catalogs)
+            {
+                Console.WriteLine(catalog.All);
+            }
+        }
+        public void Display(IEnumerable<State> states)
+        {
+            foreach (State state in states)
+            {
+                Console.WriteLine(state.All);
+            }
+        }
+        public void Display(IEnumerable<Event> events)
+        {
+            foreach (Event e in events)
+            {
+                Console.WriteLine(e.All);
+            }
+        }
+        #endregion
+
+        #region Add
+        public void AddUser(User user) => repository.AddUser(user);
+
+        public void AddCatalog(Catalog catalog) => repository.AddCatalog(catalog);
+
+        public void AddEvent(Event e) => repository.AddEvent(e);
+
+        public void AddState(State state) => repository.AddState(state);
+        #endregion
+
+        #region Create
+        public void AddUser(string firstName, string lastName, string uuid) => repository.AddUser(new User(firstName, lastName, uuid));
+
+        public void AddCatalog(string name, string genus, int height) => repository.AddCatalog(new Catalog(name, genus, height));
+
+        public void AddEvent(User user, State state, DateTime purchaseDate) => repository.AddEvent(new Event(user, state, purchaseDate));
+
+        public void AddState(Catalog catalog, int amount, double price) => repository.AddState(new State(catalog, amount, price));
+        #endregion
+
     }
 }

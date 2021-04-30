278c310a9f7346110c0a4ede3e1ce5da032c41e7
ofchar <matowczarek@outlook.com>
2021-04-30 11:49:42 +0200

    Basic project structure for task2 (i think)

---
+++ Task2/DataTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

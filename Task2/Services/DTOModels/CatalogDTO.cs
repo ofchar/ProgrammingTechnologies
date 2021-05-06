77c2c71f903d109811e314a283a2c1731738272d
ofchar <matowczarek@outlook.com>
2021-05-06 13:10:25 +0200

    Added DTO models

---
+++ Task2/Services/DTOModels/CatalogDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOModels
+{
+    public class CatalogDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genus { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ Task2/Services/DTOModels/EventDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOModels
+{
+    public class EventDTO
+    {
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public bool IsStocking { get; set; }
+
+        public int Amount { get; set; }
+
+        public int Catalog_id { get; set; }
+
+        public int User_id { get; set; }
+    }
+}
